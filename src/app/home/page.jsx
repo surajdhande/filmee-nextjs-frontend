c6286c74031756642cb019a6acc1dcfca6273dd7
Suraj <surajdhande1999@gmail.com>
2026-06-20 12:52:49 +0530

    Updated with frontend boilerplate code with next js

---
+++ src/app/home/page.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Home from '../page'
+import HomeHeroSection from '@/components/homeherosection'
+
+const HomePage = () => {
+  return (
+    <div className='text-[30px] '>
+      This is navigated home page home page
+      <HomeHeroSection/>
+    </div>
+  )
+}
+
+export default HomePage
---
+++ src/components/homeherosection/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HomeHeroSection = () => {
+  return (
+    <div>
+      This is the hero section
+    </div>
+  )
+}
+
+export default HomeHeroSection
